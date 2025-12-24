f76b3f3ce0774245b9c32ff4ad6df320d0e60f39
leroyiq <leroyiq6@gmail.com>
2025-12-24 13:16:47 +0200

    Set BrowserRouter basename to /sanX
    Updated the BrowserRouter basename from an empty string to '/sanX' in index.js to ensure correct routing under the /sanX path.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from 'App';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter basename='' >
+  <BrowserRouter basename="/sanX">
     <App />
-</BrowserRouter>
+  </BrowserRouter>
 );
